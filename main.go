bfdb04fc1d34007f8225b5a5ca33702e788452d3
Gram <master_fess@mail.ru>
2018-04-12 15:34:17 +0500

    more cohesion

--- main.go
+++ main.go
@@ -23,58 +23,72 @@ func main() {
 
 	flag.Parse()
 
-	var projects []Project
-
 	if lang != "" {
-		// download and save JSON from github
-		wg.Add(pages)
-		var projectsChan = make(chan Project, pages*100)
-		for page := 1; page <= pages; page++ {
-			go getProjects(lang, page, &projectsChan)
-		}
-		wg.Wait()
-		close(projectsChan)
-		for project := range projectsChan {
-			projects = append(projects, project)
-		}
-		b, err := json.Marshal(projects)
+		data, err := dumpProjects(lang, pages)
 		if err != nil {
-			fmt.Println(err)
+			os.Stderr.WriteString(err.Error())
 			return
 		}
-		os.Stdout.Write(b)
+		os.Stdout.Write(data)
 	} else {
-		// generate markdown
+		var projects []Project
 		// decode JSON
 		if err := json.NewDecoder(os.Stdin).Decode(&projects); err != nil {
 			fmt.Println(err)
 			return
 		}
-		// group projects by topics
-		topics := make(map[string][]Project)
-		var topicsNames []string
-		for _, project := range projects {
-			for _, topic := range project.Topics {
-				if topics[topic] == nil {
-					topicsNames = append(topicsNames, topic)
-				}
-				topics[topic] = append(topics[topic], project)
+		makeMarkdown(projects)
+	}
+}
+
+// download and save JSON from github
+func dumpProjects(lang string, pages int) ([]byte, error) {
+	wg.Add(pages)
+	var projectsChan = make(chan Project, pages*100)
+	for page := 1; page <= pages; page++ {
+		go getProjects(lang, page, &projectsChan)
+	}
+	wg.Wait()
+	close(projectsChan)
+
+	var projects []Project
+	for project := range projectsChan {
+		projects = append(projects, project)
+	}
+	dump, err := json.Marshal(projects)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return dump, nil
+}
+
+// generate markdown
+func makeMarkdown(projects []Project) {
+	// group projects by topics
+	topics := make(map[string][]Project)
+	var topicsNames []string
+	for _, project := range projects {
+		for _, topic := range project.Topics {
+			if topics[topic] == nil {
+				topicsNames = append(topicsNames, topic)
 			}
+			topics[topic] = append(topics[topic], project)
 		}
-		// sort topics
-		sort.Strings(topicsNames)
-		// generate markdown
-		var topicProjects []Project
-		for _, topicName := range topicsNames {
-			topicProjects = topics[topicName]
-			if len(topicProjects) > 1 {
-				fmt.Printf("\n\n## %s\n\n", topicName)
-				slice.Sort(topicProjects[:], func(i, j int) bool {
-					return topicProjects[i].Stars > topicProjects[j].Stars
-				})
-				for _, project := range topicProjects {
-					fmt.Println(project.getMarkdown())
-				}
+	}
+	// sort topics
+	sort.Strings(topicsNames)
+	// generate markdown
+	var topicProjects []Project
+	for _, topicName := range topicsNames {
+		topicProjects = topics[topicName]
+		if len(topicProjects) > 1 {
+			fmt.Printf("\n\n## %s\n\n", topicName)
+			slice.Sort(topicProjects[:], func(i, j int) bool {
+				return topicProjects[i].Stars > topicProjects[j].Stars
+			})
+			for _, project := range topicProjects {
+				fmt.Println(project.getMarkdown())
 			}
 		}
 	}
@@ -82,6 +96,7 @@ func main() {
 
 var wg sync.WaitGroup
 
+// endpoint for Github API search
 const searchAPI = "https://api.github.com/search/repositories"
 const projectLinkTemplate = "https://github.com/%s/%s"
 
@@ -103,14 +118,17 @@ type ProjectImport struct {
 	Topics []string `json:"topics"`
 }
 
+// make project link
 func (p *Project) getLink() string {
 	return fmt.Sprintf(projectLinkTemplate, p.Author, p.Name)
 }
 
+// make markdown block with project info
 func (p *Project) getMarkdown() string {
 	return fmt.Sprintf("1. [%s](%s). %s", p.Name, p.getLink(), p.Descr)
 }
 
+// filter slice by key function
 func filter(vs []string, f func(string) bool) []string {
 	vsf := make([]string, 0)
 	for _, v := range vs {
@@ -121,6 +139,7 @@ func filter(vs []string, f func(string) bool) []string {
 	return vsf
 }
 
+// retrieve and extract projects from Github API
 func getProjects(lang string, page int, projectsChan *chan Project) {
 	defer wg.Done()
 
